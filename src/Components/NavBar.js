b0371038115223a25f1d9d9e1af405e8f91e1707
Ryan Nicoletti <r.nicoletti1@gmail.com>
2019-11-04 16:26:54 -0500

    added title to main page of app

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -5,68 +5,69 @@ import { faBars } from "@fortawesome/free-solid-svg-icons";
 import "./NavBar.css";
 
 export default class NavBar extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { navBar_class: "" };
+  constructor(props) {
+    super(props);
+    this.state = { navBar_class: "" };
+  }
+
+  setToggleNavBarClass = e => {
+    e.preventDefault();
+    if (this.state.navBar_class === "") {
+      this.setState({ navBar_class: "toggled" });
+    } else {
+      this.setState({ navBar_class: "" });
     }
+  };
 
-    setToggleNavBarClass = e => {
-        e.preventDefault();
-        if (this.state.navBar_class === "") {
-            this.setState({ navBar_class: "toggled" });
-        } else {
-            this.setState({ navBar_class: "" });
-        }
-    };
+  render() {
+    let topNavBarClass = `top-menu-${this.state.navBar_class}`;
+    return (
+      <nav className="nav-container">
+        <div className={topNavBarClass}>
+          <ul className="nav-item-list">
+            <li>
+              <Link className="nav-links" to="/">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link className="nav-links" to="/Createaccount">
+                Create Account
+              </Link>
+            </li>
+            <li>
+              <Link className="nav-links" to="/Login">
+                Login
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <FontAwesomeIcon
+          icon={faBars}
+          size="lg"
+          className="top-menu-icon"
+          onClick={e => this.setToggleNavBarClass(e)}
+        />
 
-    render() {
-        let topNavBarClass = `top-menu-${this.state.navBar_class}`;
-        return (
-            <nav className="nav-container">
-                <div className={topNavBarClass}>
-                    <ul className="nav-item-list">
-                        <li>
-                            <Link className="nav-links" to="/">
-                                Home
-                            </Link>
-                        </li>
-                        <li>
-                            <Link className="nav-links" to="/Createaccount">
-                                Create Account
-                            </Link>
-                        </li>
-                        <li>
-                            <Link className="nav-links" to="/Login">
-                                Login
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
-                <FontAwesomeIcon
-                    icon={faBars}
-                    size="lg"
-                    className="top-menu-icon"
-                    onClick={e => this.setToggleNavBarClass(e)}
-                />
-
-                <ul className="top-nav-item-list">
-                    <li>
-                        <Link className="top-nav-links" to="/">
-                            Home
-                        </Link>
-                    </li>
-                    <li>
-                        <Link className="top-nav-links" to="/Createaccount">
-                            Create Account
-                        </Link>
-                    </li>
-                    <li>
-                        <Link className="top-nav-links" to="/Login">
-                            Login
-                        </Link>
-                    </li>
-                </ul>
-            </nav>
-        );
-    }
+        <ul className="top-nav-item-list">
+          <li className="app-name">Newsletter Inbox</li>
+          <li>
+            <Link className="top-nav-links" to="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link className="top-nav-links" to="/Createaccount">
+              Create Account
+            </Link>
+          </li>
+          <li>
+            <Link className="top-nav-links" to="/Login">
+              Login
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
 }
